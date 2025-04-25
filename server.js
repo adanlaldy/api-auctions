6b2178c91d7c78be39b7853b5c97d8b9fb5a9932
DariusDreem <reungoat.nathan@gmail.com>
2025-04-25 08:26:17 +0200

    added create to doc

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import app from './app.js'
+import cors from 'cors'
 
 const port = 3000
 const hostname = '0.0.0.0'
+
+app.use(cors());
 
 app.listen(port, hostname, () => {
     console.log('\x1b[32m%s\x1b[0m', `Server running at http://localhost:${port}/`)
